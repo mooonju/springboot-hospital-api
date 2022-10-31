d9e8a05ebfeb480af18a1b631d23b2efe11f72a3
MUNJU <mvv0030@gmail.com>
2022-10-31 23:19:37 +0900

    parser 인터페이스 추가

---
+++ exercise1/src/main/java/com/likelion/domain/Hospital.java
@@ -0,0 +1,34 @@
+package com.likelion.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+public class Hospital {
+    private int id;
+    private String openServiceName;
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+
+
+
+}
